aead74d7e90744b41d630dbc4282bed942ab430a
OlgaRybachenko <rybachenkoolga@yandex.by>
2023-11-16 14:44:00 +0300

    working with layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,174 @@
 <script setup lang="ts">
-
+  // import { ref, computed} from 'vue' 
+  // import { API_KEY, BASE_URL } from "@/apiCon"
+   import WeatherHeader from '@/components/WeatherHeader.vue'
+  // import BigBlock from '@/components/BigBlock.vue'
+  
 </script>
 
 <template>
- 
+ <div class="page">
+       <main class="main">
+        <div class="container">
+          <div class="tablet">
+            <div class="sections">
+              <section class="section section_left">
+                <img src="./assets/imgvue/Logo.png" alt="" class="section-left_logo">
+              </section>
+              <section class="section_right">
+                <div class="info">
+                  <div class="city-inner">
+                    <input type="text" class="search">
+                  </div>
+                  <WeatherHeader />
+                </div>
+              </section>
+              <section class="section section-right">
+                <!-- <BigBlock /> -->
+              </section> 
+            </div>
+            <!--<div class="sections">
+              <section class="section-bottom">
+                <div
+                  class="block-bottom"
+                >
+                  <div class="block-bottom-inner">
+                    <div class="block-bottom-pic pic-coords"></div>
+                    <div class="block-bottom-texts">
+                      <div class="block-bottom-text-block">
+                        <div class="block-bottom-text-block-title">
+                          Longitude: 2.3488
+                        </div>
+                        <div class="block-bottom-text-block-desc">
+                          Longitude measures distance east or west of the prime meridian.
+                        </div>
+                      </div>
+                      <div class="block-bottom-text-block">
+                        <div class="block-bottom-text-block-title">
+                          Latitude: 48.8534
+                        </div>
+                        <div class="block-bottom-text-block-desc">
+                          Latitude lines start at the equator (0 degrees latitude) and run east and west, parallel to the equator. 
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section class="section-bottom">
+                <div
+                  class="block-bottom"
+                >
+                  <div class="block-bottom-inner">
+                    <div class="block-bottom-pic pic-humidity"></div>
+                    <div class="block-bottom-texts">
+                      <div class="block-bottom-text-block">
+                        <div class="block-bottom-text-block-title">
+                          Humidity: 60 %
+                        </div>
+                        <div class="block-bottom-text-block-desc">
+                          Humidity is the concentration of water vapor present in the air. Water vapor, the gaseous state of water, is generally invisible to the human eye.
+                          <br /><br />
+                          The same amount of water vapor results in higher relative humidity in cool air than warm air.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>-->
+          </div>
+        </div>
+      </main> 
+    </div>
 </template>
 
-<style scoped>
+<style lang="sass" scoped>
+@import '@/assets/styles/main'
+.page
+  position: relative
+  display: flex
+  justify-content: center
+  align-items: center
+  min-height: 100vh
+  padding: 20px 0
+  background-color: #000
+
+.tablet
+  width: 100%
+  padding: 20px
+  background: url('@/assets/imgvue/Frame1.png')
+  background-repeat: no-repeat
+  background-size: cover
+
+.sections
+  display: flex
+  width: 100%
+
+  @media (max-width: 767px)
+    flex-direction: column
+
+.section_left
+  width: 50%
+  padding-right: 10px
+
+  @media (max-width: 767px)
+    width: 100%
+    padding-right: 0
+
+.section-left_logo
+  padding-top: 62px
+  padding-left: 30px
+
+.section-right
+  width: 50%
+  padding-left: 10px
+
+  @media (max-width: 767px)
+    width: 100%
+    margin-top: 16px
+    padding-left: 0
+
+.city-inner
+  // position: relative
+  // display: inline-block
+  width: 100%
+
+  &::after
+    content: ''
+    position: absolute
+    top: 0
+    right: 10px
+    width: 25px
+    height: 25px
+    background: url('./assets/img/search.svg') no-repeat 50% 50%
+    background-size: contain
+    transform: translateY(50%)
+    cursor: pointer
+
+.info
+  height: 100%
+  padding: 16px
+  background: url('./assets/img/gradient-1.jpg') no-repeat 50% 50%
+  background-size: cover
+  border-radius: 25px
+
+.search
+  width: 100%
+  padding: 16px
+  font-family: 'Montserrat', sans-serif 
+  color: #000
+  background-color: #fff
+  border-radius: 25px
+  border: none
+  outline: none
+  cursor: pointer
+
+.section-bottom
+  width: 50%
+  margin-top: 16px
+
+  @media (max-width: 767px)
+    width: 100%
 
 </style>
---
+++ src/apiCon/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'b7c5c3aad5f6cdceebec4b1e9e89b6bc'
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
